1f5c5397f75c5fb072826731fd9c9d3b0c85de1b
unknown <mattgrossfeld@gmail.com>
2025-02-24 00:36:54 -0600

    Shouts updated

--- controllers/shoutsController.js
+++ controllers/shoutsController.js
@@ -3,12 +3,9 @@ const shoutsService = require('../services/shoutsService');
 
 const router = express.Router();
 
-// Single thread
 router.get('/:shoutId', shoutsService.getShoutById);
 router.put('/:shoutId', shoutsService.updateShoutById);
 router.delete('/:shoutId', shoutsService.deleteShoutById);
-
-// Multiple threads
 router.get('/', shoutsService.getShouts);
 router.post('/', shoutsService.insertShout);
 
--- queries/shoutsQueries.js
+++ queries/shoutsQueries.js
@@ -1,7 +1,7 @@
 module.exports = Object.freeze({
     GET_SHOUT_BY_ID: ' SELECT * FROM forum.shouts where id = $1; ',
     GET_SHOUTS: ' SELECT * FROM forum.shouts; ',
-    INSERT_SHOUT: ' insert into forum.shouts (shout, created_by, created_tm, modified_by, modified_tm) values ($1, $2, $3, $4, $5); ',
-    UPDATE_SHOUT_BY_ID: ' UPDATE forum.shouts SET shout = $1, created_by = $2, created_tm = $3, modified_by = $4, modified_tm = $5 WHERE ID = $6; ',
+    INSERT_SHOUT: ' insert into forum.shouts (shout, community_id, created_by, created_tm, modified_by, modified_tm) values ($1, $2, $3, CURRENT_TIMESTAMP, $4, CURRENT_TIMESTAMP); ',
+    UPDATE_SHOUT_BY_ID: ' UPDATE forum.shouts SET shout = $1, modified_by = $4, modified_tm = CURRENT_TIMESTAMP WHERE ID = $5; ',
     DELETE_SHOUT_BY_ID: ' DELETE FROM forum.shouts WHERE id = $1; '
 });
